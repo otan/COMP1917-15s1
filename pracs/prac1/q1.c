d63944e74a7f0a19684b165e3c9917e9462cb8b2
Oliver Tan <sinovercosequalstan@gmail.com>
2015-03-31 08:48:29 +1100

    added prac 1

---
+++ pracs/prac1/q1.c
@@ -0,0 +1,40 @@
+/*
+ * Write a program that takes in a number, and prints all numbers from 1 to that number
+ * inclusive that is either odd, or divisible by 5.
+ *
+ * For example:
+ *
+ * oliver@computer: ./prac1 
+ * Enter a number: 13
+ * 1
+ * 3
+ * 5
+ * 7
+ * 9
+ * 10
+ * 11
+ * 13
+ *
+ * Compile using: gcc -Wall -Werror -O -o q1 q1.c
+ * This can be automarked, running: ./mark q1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Enter a number: ");
+    // TODO(you): finish off this program!
+
+    int n;
+    scanf("%d", &n);
+    int i = 1;
+    while (i <= n) {
+        if (i % 2 == 1 || i % 5 == 0) {
+            printf("%d\n", i);
+        }
+        i++;
+    }
+
+    return EXIT_SUCCESS;
+}
