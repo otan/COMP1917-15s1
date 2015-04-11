e4f37d0019591affde1ec1b4164e35fd25fc0bfb
Oliver Tan <sinovercosequalstan@gmail.com>
2015-04-11 21:47:55 +1000

    clarification

--- pracs/prac2/q1.c
+++ pracs/prac2/q1.c
@@ -22,6 +22,7 @@
  * 1 2 3 4 5 6 7 8 9 10 
  * 11 12 13 14 15 16 17 18 19 20 
  * 21 22 23 
+ * oliver@computer: 
  *
  * Compile using: gcc -Wall -Werror -O -o q1 q1.c
  * This can be automarked, running: ./mark q1
