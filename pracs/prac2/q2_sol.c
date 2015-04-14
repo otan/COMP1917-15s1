c78b780d26e271e7a10bb2fd88310fe808406d69
Oliver Tan <sinovercosequalstan@gmail.com>
2015-04-14 13:33:51 +1000

    q1/2 sol

---
+++ pracs/prac2/q2_sol.c
@@ -0,0 +1,66 @@
+/*
+   Write a program that prints a square banner!
+   This is when on each even row, each even number (from 0) is starred, and
+        on each odd row, each odd number is starred.
+   An example of what it looks like is below.
+ 
+   oliver@computer: ./q2
+   Enter a number: 1
+   *
+   oliver@computer: ./q2
+   Enter a number: 2
+   *
+    *
+   oliver@computer: ./q2
+   Enter a number: 3
+   * *
+    *
+   * *
+   oliver@computer: ./q2
+   Enter a number: 5
+   * * *
+    * * 
+   * * *
+    * * 
+   * * *
+   oliver@computer: ./q2
+   Enter a number: 8
+   * * * * 
+    * * * *
+   * * * * 
+    * * * *
+   * * * *  
+    * * * *
+   * * * * 
+    * * * *
+
+   Compile using: gcc -Wall -Werror -O -o q2 q2.c
+   This can be automarked, running: ./mark q2
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int row, col;
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // TODO(you): finish off this program!
+    row = 0;
+    while (row < n) {
+        col = 0;
+        while (col < n) {
+            if ((row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+            col++;
+        }
+        printf("\n");
+        row++;
+    }
+    return EXIT_SUCCESS;
+}
