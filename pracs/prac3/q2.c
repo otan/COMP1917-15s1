364749ed125f203f78b4258d2d52bf24527dd99e
Oliver Tan <sinovercosequalstan@gmail.com>
2015-04-17 23:18:33 +1000

    practice test

---
+++ pracs/prac3/q2.c
@@ -0,0 +1,46 @@
+/*
+ * Past practical exam question in 2014s1
+ *
+ * Given an odd number, print out an X
+ *
+ * NOTE: Each line should have the odd number of characters.
+ *
+ * I will NOT test even number cases.
+ *
+ * oliver@computer: ./q2
+ * Enter a number: 1
+ * #
+ * oliver@computer: ./q2
+ * Enter a number: 3
+ * # #
+ *  #
+ * # #
+ * oliver@computer: ./q2
+ * Enter a number: 5
+ * #   #
+ *  # #
+ *   #
+ *  # #
+ * #   #
+ * oliver@computer: ./q2 9
+ * #       #
+ *  #     #
+ *   #   #
+ *    # #
+ *     #
+ *    # #
+ *   #   #
+ *  #     #
+ * #       #
+ *
+ * Compile using: gcc -Wall -Werror -O -o q2 q2.c
+ * Run using: ./mark q2
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("asian father is sad this printf statement is still here.\n");
+    return EXIT_SUCCESS;
+}
